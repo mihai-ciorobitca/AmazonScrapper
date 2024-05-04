02617c71eeac9b4f919607eb897066b8897f550a
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-05-04 11:53:29 +0300

    upload

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
-git init
-git remote add origin https://github.com/mihai-ciorobitca/AmazonScrapper
+rem git init
+rem git remote add origin https://github.com/mihai-ciorobitca/AmazonScrapper
 git add .
 git commit -m "upload"
 git push origin master
