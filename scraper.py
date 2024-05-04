b3229f02d618fc8558a855f5a15a7e4ca00c71fa
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-05-04 11:50:46 +0300

    upload

---
+++ scraper.py
@@ -0,0 +1,9 @@
+# import section
+
+
+def main():
+    urls = [
+        "https://www.amazon.sa/-/en/Apple-20W-USB-C-Power-Adapter/dp/B08L85KDV5/ref=zg_bs_g_electronics_d_sccl_1/260-1454209-2227524?psc=3"
+    ]
+
+main()
